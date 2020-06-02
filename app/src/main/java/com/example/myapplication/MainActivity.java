2eb467302ac70226f2753523b15895b413755899
UzairRehman <uzairrehman49@yahoo.com>
2020-06-02 17:14:16 +0500

    final commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+RatingBar ratingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratingBar = findViewById(R.id.ratingBar);
+        ratingBar .setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+               Toast.makeText(MainActivity.this,"Your Rating : " + rating,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
